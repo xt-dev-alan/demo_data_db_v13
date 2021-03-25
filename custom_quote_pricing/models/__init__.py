f2dad9711191b5147ce90058851a99d10ebc029a
xt-dev-alan <61937464+xt-dev-alan@users.noreply.github.com>
2021-03-24 18:25:43 -0600

    renew DB

--- custom_quote_pricing/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sale_quote_pricing
-from . import sale
-
--- quality_mrp/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import production
-from . import qc_inspection
-from . import sale
